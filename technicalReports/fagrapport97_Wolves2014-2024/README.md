0b6f8ad16cf34a64817b05eb6feaf074ed53ce0f
Cyril-Milleret <cyril.milleret@gmail.com>
2024-06-03 15:48:23 +0200

    push material related to fagrapport 97 (wolf report 2024)

---
+++ technicalReports/fagrapport97_Wolves2014-2024/README.md
@@ -0,0 +1,8 @@
+# fagrapport97_Wolves2014-2024
+
+Repository containing the input data, R script, figures, tables and raster density maps from the following technical report: 
+ Milleret, C., Dupont, P., Brøseth, H., Flagstad, Ø., Kindberg, J., Svensson, L., and Bischof, R., 2024.
+Estimates of wolf density, abundance, and population dynamics in Scandinavia, 2014–2024 MINA fagrapport 97. 34 pp.[PDF LINK](https://www.researchgate.net/publication/373106889_Estimates_of_wolf_density_abundance_and_population_dynamics_in_Scandinavia_2013-2023)
+ 
+
+
---
+++ technicalReports/fagrapport97_Wolves2014-2024/RScripts/runNimbleModel.R
@@ -0,0 +1,41 @@
+library(nimble)
+library(nimbleSCR)
+##----  1. OPSCR models ----
+rm(list=ls())
+setwd("C:/My_documents/rovquant/analyses/Rgit/RovQuantPublic/technicalReports/fagrapport97_Wolves2014-2024/input/OPSCR")
+
+# load the sex-specific input data
+# Females
+load("36.F_2023_sf_INPUTChain1.RData")
+# Males
+#load("36.M_2023_sf_INPUTChain1.RData")
+
+
+## load a custom nimble function located in fagrapport74_Wolverine2013-2023/RScripts
+setwd("C:/My_documents/rovquant/analyses/Rgit/RovQuantPublic/technicalReports/fagrapport97_Wolves2014-2024/RScripts")
+source("dbinomLocal_normalWolf.R")
+
+model <- nimbleModel( code = modelCode,
+                      constants = nimConstants,
+                      data = nimData,
+                      inits = nimInits,
+                      check = F,
+                      calculate = F)
+model$calculate()
+cmodel <- compileNimble(model)
+cmodel$calculate()
+MCMCconf <- configureMCMC(model = model,
+                          monitors = nimParams,
+                          control = list(reflective = TRUE),
+                          thin = 1)
+
+MCMC <- buildMCMC(MCMCconf)
+cMCMC <- compileNimble(MCMC, project = model, resetFunctions = TRUE)
+# RUN THE MCMC 
+MCMCRuntime <- system.time(myNimbleOutput <- runMCMC( mcmc = cMCMC,
+                                                      nburnin = 1,
+                                                      niter = 10,
+                                                      nchains = 1,
+                                                      samplesAsCodaMCMC = TRUE))
+overallRuntimeEnd <- proc.time()
+
