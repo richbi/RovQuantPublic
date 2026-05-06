2efb3ad31716be0813efb5f6cb2af8de4f278be5
Cyril Pierre Milleret <cyril.pierre.milleret@nmbu.no>
2026-05-06 11:36:51 +0200

    upload all tables and figures

--- technicalReports/fagrapport112_Wolverine2016-2025/RScripts/runNimbleModel.R
+++ technicalReports/fagrapport112_Wolverine2016-2025/RScripts/runNimbleModel.R
@@ -2,17 +2,24 @@ library(nimble)
 library(nimbleSCR)
 ##----  1. OPSCR models ----
 rm(list=ls())
-setwd("C:/My_documents/rovquant/analyses/Rgit/RovQuantPublic/technicalReports/fagrapport101_Wolverine2015-2024/input/OPSCR")
-
+#FOR OPSCR MODELS 
+setwd("C:/My_documents/rovquant/analyses/Rgit/RovQuantPublic/technicalReports/fagrapport112_Wolverine2016-2025/input/OPSCR")
 # load the sex-specific input data
 # Females
-load("53.aJ_FaCleaned2024Chain1.RData")
+load("54.Cleaned2025Hunn_Chain1.RData")
 # Males
-#load(""53.aJ_MaCleaned2024Chain1.RData")
+#load("54.Cleaned2025Hann_Chain1.RData")
 
+#FOR SCR MODELS 
+setwd("C:/My_documents/rovquant/analyses/Rgit/RovQuantPublic/technicalReports/fagrapport112_Wolverine2016-2025/input/SCR")
+# load the sex-specific input data
+# Females
+# load("Snap2024_54.Cleaned2025Hunn_Chain1.RData")
+# Males
+#load("Snap2024_54.Cleaned2025Hann_Chain1.RData")
 
 ## load a custom nimble function located in fagrapport89_Wolverine2014-2023/RScripts
-setwd("C:/My_documents/rovquant/analyses/Rgit/RovQuantPublic/technicalReports/fagrapport89_Wolverine2014-2023/RScripts")
+setwd("C:/My_documents/rovquant/analyses/Rgit/RovQuantPublic/technicalReports/fagrapport112_Wolverine2016-2025/RScripts")
 source("dbin_LESS_Cached_MultipleCovResponse.R")
 
 model <- nimbleModel( code = modelCode,
